c93ed4a5526bcd08ffbf93e6606a55975d86dc7e
clash_m45 <clash.m45@gmail.com>
2016-01-25 23:50:57 +0900

    機能追加 保存するファイル名に日付を使う。日記に改行を入れることが出来る。

--- src/main/java/me/m45/diary/ConsoleDiary.java
+++ src/main/java/me/m45/diary/ConsoleDiary.java
@@ -1,26 +1,32 @@
 package me.m45.diary;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.CREATE;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ConsoleDiary {
 
 	public static void main(String[] args) throws IOException {
+		String fileName = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE)+".md";
 		try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 				BufferedWriter writer = Files.newBufferedWriter(
-						Paths.get("build", "firstdialy.md"),
-						StandardCharsets.UTF_8,
-						StandardOpenOption.CREATE) ) {
-			writer.write(reader.readLine());
+						Paths.get("build", fileName), UTF_8, CREATE) ) {
+			while(true) {
+				String str = reader.readLine();
+				if("eol".equals(str)) {
+					break;
+				}
+				writer.write(str);
+				writer.newLine();
+			}
 		}
 	}
-
-
-
 }
