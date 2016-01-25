8c0c68178b37decab75d19266f4a6662343987ef
clash_m45 <clash.m45@gmail.com>
2016-01-25 23:56:57 +0900

    最低限機能の日記は書けるようになった。 徐々に機能を追加していこうと思う。

--- src/main/java/me/m45/diary/ConsoleDiary.java
+++ src/main/java/me/m45/diary/ConsoleDiary.java
@@ -16,9 +16,15 @@ public class ConsoleDiary {
 
 	public static void main(String[] args) throws IOException {
 		String fileName = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE)+".md";
+		String todate = "["+LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)+"]";
 		try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 				BufferedWriter writer = Files.newBufferedWriter(
 						Paths.get("build", fileName), UTF_8, CREATE) ) {
+			//今日の日付を記述
+			System.out.println(todate);
+			writer.write(todate);
+			writer.newLine();
+
 			while(true) {
 				String str = reader.readLine();
 				if("eol".equals(str)) {
